ab99da967fcf1146eaa5f43fee24ba835ac714ed
Nguyen Long <longsky0912624119@gmail.com>
2024-09-26 23:29:57 +0700

    Change greeting information at HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,31 +9,30 @@ const HeroSection = () => {
     return (
         <div className='heroSecMainParent'>
 
-            <p className='text-4xl mt-20 text-center welcomStore'>WELCOME TO OUR STORE</p>
+            <p className='text-4xl mt-20 text-center welcomStore'>HEY! FIND YOUR STYLE.</p>
             <br />
             <br />
 
 
-            <p className='heroDescription relative text-xl flex flex-wrap'> The self care brand that's setting a new standard. Natural products, to help
-                you feel good in your own body. </p>
+            <p className='heroDescription relative text-xl flex flex-wrap'> If you don't conform to the way others dress, you don't have to conform to their way of thinking. </p>
 
 
             <div className='heroPicHold flex flex-row '>
-                <img src={one} className=" w-20" />
+                <img src={one} className=" w-20 " />
                 <img src={two} className=" w-20" />
                 <img src={three} className=" w-20" />
             </div>
 
             <div className=' text-xl font-medium flex flex-row heroHeadingHold'>
-                <p> Powered By Nature </p>
-                <p> Exceptional Quality </p>
-                <p> Skin First Formulas </p>
+                <p> Made to Last </p>
+                <p> Crafted for Comfort </p>
+                <p> Soft on Your Skin </p>
             </div>
 
             <div className='heroDescHold flex-row flex relative text-base font-normal'>
-                <p> Aliquam venenatis vel lorem id <br /> nuaer mollis eget. </p>
-                <p> Nulla tincidunt gravida augue, eget <br /> pellentesque. </p>
-                <p> Aliquam sed diam fringilla, loborti <br /> sorci nec </p>
+                <p> Our jeans are crafted with durable fabrics and meticulous stitching, ensuring they stand the test of time.  </p>
+                <p> Our shirts are designed with your comfort in mind. We use soft, breathable fabrics and tailored cuts for a perfect fit. </p>
+                <p> Our shirts are made from gentle, skin-friendly fabrics that feel luxurious against your skin. Enjoy ultimate comfort and a soft touch. </p>
             </div>
         </div>
     )
