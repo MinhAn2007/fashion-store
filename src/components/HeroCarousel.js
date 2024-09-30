259daf5ec68708b34020ab49e4e3dfd48ace8515
minhan <voongocminhan20072002@gmail.com>
2024-09-30 15:27:20 +0700

    auto slide

--- src/components/HeroCarousel.js
+++ src/components/HeroCarousel.js
@@ -3,28 +3,30 @@ import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import banner1 from "../assets/banner1.png"
-import banner2 from "../assets/banner2.png"
-import banner3 from "../assets/banner3.png"
-export default class SimpleSlider extends Component {
+import banner1 from "../assets/banner1.png";
+import banner2 from "../assets/banner2.png";
+import banner3 from "../assets/banner3.png";
 
+export default class SimpleSlider extends Component {
   render() {
     const settings = {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      autoplay: true,          
+      autoplaySpeed: 3000,     
     };
+
     return (
       <div className="carousel-contain">
         <Slider {...settings}>
-        <div>
-        <img src={banner2} className="car-pic" alt="Banner 1" />
+          <div>
+            <img src={banner2} className="car-pic" alt="Banner 1" />
           </div>
           <div>
             <img src={banner3} className="car-pic" alt="Banner 2" />
           </div>
-
           <div>
             <img src={banner1} className="car-pic" alt="Banner 3" />
           </div>
