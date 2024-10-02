3c7501095ba1374841b5e6c211ebb2ea9284d8cc
Nguyen Long <longsky0912624119@gmail.com>
2024-10-03 01:00:04 +0700

    temporary change vietnamese to english at footer

--- src/components/SPFooter.js
+++ src/components/SPFooter.js
@@ -3,26 +3,26 @@ import React from 'react'
 const SPFooter = () => {
     return (
         <div>
-            <footer className="footer p-10 fof text-xl fotBg text-base-content">
+            <footer className="footer p-10 fof text-xl fotBg text-base-content ">
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <span className="footer-title">DỊCH VỤ</span>
+                    <a className="link link-hover">Thương hiệu</a>
+                    <a className="link link-hover">Thiết kế</a>
+                    <a className="link link-hover">Tiếp thị</a>
+                    <a className="link link-hover">Quảng cáo</a>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <span className="footer-title">CÔNG TY</span>
+                    <a className="link link-hover">Giới thiệu về chúng tôi</a>
+                    <a className="link link-hover">Liên hệ</a>
+                    <a className="link link-hover">Tuyển dụng</a>
+                    <a className="link link-hover">Bộ thông tin báo chí</a>
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <span className="footer-title">Luật pháp</span>
+                    <a className="link link-hover">Điều khoản sử dụng</a>
+                    <a className="link link-hover">Chính sách bảo mật</a>
+                    <a className="link link-hover">Chính sách Cookie </a>
                 </div>
             </footer>
             <footer className="footer px-10 py-4 border-t fotBg text-base-content border-base-300">
